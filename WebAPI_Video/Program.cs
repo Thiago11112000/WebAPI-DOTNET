cdfd5dd9d05266c795d3f4d56842c01fe403cf1a
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-05-11 14:45:08 -0300

    Adicionando cors

--- WebAPI_Video/Program.cs
+++ WebAPI_Video/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using WebAPI_Video.DataContext;
-using WebAPI_Video.Service;
 using WebAPI_Video.Service.FuncionarioService;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,14 +9,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddScoped<IFuncionarioInterface, FuncionarioService>();
 builder.Services.AddSwaggerGen();
-
-
+builder.Services.AddScoped<IFuncionarioInterface, FuncionarioService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("funcionariosApp", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,7 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("funcionariosApp");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
--- WebAPI_Video/obj/Debug/net6.0/WebAPI_Video.AssemblyInfo.cs
+++ WebAPI_Video/obj/Debug/net6.0/WebAPI_Video.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI_Video")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d1e6bd7ae92ab5b9dbba50cdec0b3460277f0db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+123f7307faca8b1185308df64ec2c453b8026d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI_Video")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI_Video")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
